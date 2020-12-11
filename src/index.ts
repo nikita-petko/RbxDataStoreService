68bbf321b6a128de516a6dede817ed0508800d06
mfd <minigunneryt@gmail.com>
2020-12-11 20:03:08 +0000

    Major changes, Including a top HEAD License, better info, and TONS of more methods and classes, will be finished by christmas day

--- src/index.ts
+++ src/index.ts
@@ -1,185 +1,248 @@
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-/* eslint-disable @typescript-eslint/no-empty-function */
-/* eslint-disable @typescript-eslint/no-namespace */
+/* 
+	FileName: index.ts
+	Written By: Nikita Nikolaevich Petko, - nikita-mfd
+	File Type: Module
+	Description: The main file to implement DataStoreService as a NodeJS.
+	
+	***
+
+	Copyright 2015-2020 MFD
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+
+	***
+*/
+import { IncomingMessage as Response } from 'http';
 import r from 'request-promise';
-import dns from 'dns';
-const baseUrl = 'https://{subDomain}.roblox.com/{resource}';
-const apis = {
-	gamepersistence: baseUrl.replace('{subDomain}', 'gamepersistence'),
+// NOTICE Of removing backendProcessing,
+// throttleCounterGets,
+// throttleCounterGetSorted,
+// throttleCounterSets,
+// throttleCounterOrderedSets,
+// throttledGets,
+// throttledGetSorted,
+// throttledSets,
+// throttledOrderedSets
+
+const globals = {
+	cookie: '',
+	placeId: 0,
 };
-const endpoints = {
-	get: apis.gamepersistence.replace('{resource}', 'persistence/getV2'),
-	set: apis.gamepersistence.replace('{resource}', 'persistence/set'),
+const cookieWarningCapture: RegExp = /_\|WARNING:-DO-NOT-SHARE-THIS\.--Sharing-this-will-allow-someone-to-log-in-as-you-and-to-steal-your-ROBUX-and-items\.\|_/;
+
+const checkNameAndScope = (name: string, scope: string) => {
+	if (scope.length == 0) throw new Error("DataStore scope can't be empty string");
+
+	if (scope.length > 50) throw new Error('DataStore scope is too long');
+
+	if (name.length == 0) throw new Error("DataStore name can't be empty string");
+
+	if (name.length > 50) throw new Error('DataStore name is too long');
 };
 
-const getIPFromDnsAsync = (DomainName: string): Promise<string> => {
-	return new Promise((resolve, reject) => {
-		dns.lookup(DomainName, 0, (err, result) => {
-			if (err) {
-				reject(err);
-				return;
-			}
-			resolve(result);
-		});
+const checkCookieAndPlaceIdInternalAsync = (cookie: string, placeId: number): Promise<void> => {
+	return new Promise((resolve: (value: PromiseLike<void> | void) => void, reject: (reason?: any) => void) => {
+		if (cookie.length === 0) return reject("Cookie name can't be empty");
+		if (placeId < 1) return reject('The placeId is required to at least be >1');
+		if (!cookie.match(cookieWarningCapture))
+			return reject("Cookie isn't valid, it requires the warning text to persistent");
+		r('https://users.roblox.com/v1/users/authenticated', {
+			method: 'GET',
+			headers: { Cookie: '.ROBLOSECURITY=' + cookie },
+			resolveWithFullResponse: true,
+		})
+			.catch((err) => {
+				if (err.statusCode === 401) return reject("Cookie isn't valid, it threw a 401");
+				else return reject(err.message);
+			})
+			.then(() => resolve());
 	});
 };
 
-const initConnectionWithGP = async (domain: string) => {
-	console.warn('* Verbose flag set');
-	console.log(`* Trying ${await getIPFromDnsAsync(domain.replace('https://', ''))} at port (442)...`);
-	r({ url: domain })
-		.then(async () => {
-			console.info(
-				`* Connected to ${domain.replace('https://', '')} (${await getIPFromDnsAsync(
-					domain.replace('https://', ''),
-				)}) port 443 (#0)`,
-			);
-			console.info('* ALPN, offering http/1.1');
-			console.info(
-				'* successfully set certificate verify locations:\n*   CAfile: C:/Program Files/Git/mingw64/ssl/certs/ca-bundle.crt\n CApath: none',
-			);
-			console.info(
-				`* TLSv1.3 (OUT), TLS handshake, Client hello (1):\n* TLSv1.3 (IN), TLS handshake, Server hello (2):\n* TLSv1.3 (IN), TLS handshake, Encrypted Extensions (8):\n* TLSv1.3 (IN), TLS handshake, Certificate (11):\n* TLSv1.3 (IN), TLS handshake, CERT verify (15):\n* TLSv1.3 (IN), TLS handshake, Finished (20):n* TLSv1.3 (OUT), TLS change cipher, Change cipher spec (1):\n* TLSv1.3 (OUT), TLS handshake, Finished (20):\n* SSL connection using TLSv1.3 / TLS_AES_128_GCM_SHA256\n* ALPN, server accepted to use h2\n* Server certificate:\n*  subject: OU=Domain Control Validated; CN=*.roblox.com\n*  start date: Jun 26 19:07:18 2019 GMT\n*  expire date: Jun 26 19:07:18 2021 GMT\n*  subjectAltName: host "gamepersistence.roblox.com" matched cert's "*.roblox.com"\n*  issuer: C=US; ST=Arizona; L=Scottsdale; O=GoDaddy.com, Inc.; OU=http://certs.godaddy.com/repository/; CN=Go Daddy Secure Certificate Authority - G2\n*  SSL certificate verify ok.\n* Using HTTP2, server supports multi-use\n* Connection state changed (HTTP/2 confirmed)\n* Copying HTTP/2 data in stream buffer to connection buffer after upgrade: len=0\n* Using Stream ID: 1 (easy handle 0x25801d0)`,
-			);
-		})
-		.catch((v) => {
-			console.warn('* ' + v);
-			console.warn('* Closing connection 0');
-			throw '';
-		});
+export const init = async (cookie: string, placeId: number) => {
+	await checkCookieAndPlaceIdInternalAsync(cookie, placeId);
+	globals.cookie = cookie;
+	globals.placeId = placeId;
+	// console.log(globals);
 };
 
-export namespace DataStoreService {
-	export class DataStore {
-		private _key = '';
-		private _cookie = '';
-		private _placeId = -1;
-		private _verb = false;
-		private _scope = '';
-		/**
-		 *
-		 * @param {number=} DataStoreName The name of youe datastore, like the `Key` of `DataStoreService::GetDataStore(string Key)`
-		 * @param opts Options to use, if verboseSession is set, then you'll have a very
-		 * @suppress {opts} because {opts} is not sufficiently constrained.
-		 * @closurePrimitive {asserts.truthy}
-		 */
-		public constructor(
-			DataStoreName: string,
-			opts: { cookie: string; placeId: number; verboseSession?: boolean; Scope?: string },
-		) {
-			this._key = DataStoreName;
-			this._cookie = opts.cookie;
-			this._placeId = opts.placeId;
-			this._verb = opts.verboseSession;
-			this._scope = opts.Scope;
+enum RequestType {
+	GET_ASYNC = 5,
+	UPDATE_ASYNC = 6,
+	SET_ASYNC = 7,
+	INCREMENT_ASYNC = 8,
+	GET_SORTED_ASYNC_PAGE = 9,
+}
+enum RefetchState {
+	RefetchOnUpdateKeys,
+	RefetchCachedKeys,
+	RefetchDone,
+}
+type DataStores = Map<string, DataStore>;
+type CachedKeys = Map<string, CachedRecord>;
+type KeyTimestamps = Map<string, number>;
+export abstract class DataStoreService {
+	public static readonly HttpRequest = class {
+		public key: string;
+		public url: string;
+		public postData: string;
+		public owner: DataStore;
+		public execute(dataStoreService: DataStoreService): void {
+			const http = {
+				Headers: {
+					'Cache-Control': 'no-cache',
+					Cookie: '.ROBLOSECURITY=' + globals.cookie,
+					'Content-Type': 'application/x-www-form-urlencoded',
+				},
+				resolveWithFullResponse: true,
+				method: 'POST',
+				body: this.postData.length === 0 ? ' ' : this.postData,
+			};
+			r.post(this.url, http).then((res: Response) => {});
 		}
-		public GetAsync = async (Target: string): Promise<never> => {
-			return new Promise(async (resolve, reject) => {
-				if (this._verb) {
-					await initConnectionWithGP(apis.gamepersistence.replace('/{resource}', ''));
-					r({
-						uri: endpoints.get + `?type=standard`,
-						method: 'POST',
-						form: `&qkeys[0].scope=${this._scope || 'global'}&qkeys[0].target=${Target}&&qkeys[0].key=${
-							this._key
-						}`,
-						headers: { 'Roblox-Place-Id': this._placeId, Cookie: '.ROBLOSECURITY=_||_' + this._cookie },
-						resolveWithFullResponse: true,
-						withCredentials: true,
-					})
-						.then((v) => {
-							console.info(
-								`> POST /persistence/getV2?type=standard HTTP/2\n> Host: gamepersistence.roblox.com\n> User-Agent: mfd\n> Accept: */*\n> Content-Type: application/x-www-form-urlencoded\n> Connection: keep-alive\n> \n> &qkeys[0].scope=${
-									this._scope || 'global'
-								}&qkeys[0].target=${Target}&&qkeys[0].key=${this._key}`,
-							);
-							const x = JSON.parse(v.body);
+		public requestType: RequestType;
+	};
+	private static readonly dataStores: DataStores;
+	private static readonly orderedDataStores: DataStores;
+	private static getDataStoreInternal(name: string, scope: string, legacy: boolean, ordered: boolean): DataStore {
+		if (legacy) {
+			if (!this.legacyDataStore) {
+				this.legacyDataStore = new DataStore(name, scope, true);
+			}
+			return this.legacyDataStore;
+		} else if (ordered) {
+			const key = `${name}-${scope}`;
+			const it = this.orderedDataStores.has(key);
+			if (it === false) {
+				const ds = new OrderedDataStore(name, scope);
+				this.orderedDataStores[key] = ds;
+				return ds;
+			}
+			return this.orderedDataStores[key];
+		} else {
+			const key = `${name}-${scope}`;
+			const it = this.dataStores.has(key);
+			if (it === false) {
+				const ds = new DataStore(name, scope, false);
+				this.dataStores[key] = ds;
+				return ds;
+			}
+			return this.dataStores[key];
+		}
+	}
 
-							if (x.data) if ((x.data as []).length === 0) return reject('The key requested is null.');
-							console.info(`< HTTP/2 ${v.statusCode}`);
-							Object.keys(v.headers).forEach((value) => console.info(`< ${value}: ${v.headers[value]}`));
-							console.info(`${v.body}* Connection #0 to host gamepersistence.roblox.com left intact`);
-							return resolve(x.data[0].Value);
-						})
-						.catch((v) => reject(JSON.parse(v.error).errors[0].message));
-				} else {
-					r({
-						uri: endpoints.get + `?type=standard`,
-						method: 'POST',
-						form: `&qkeys[0].scope=${this._scope || 'global'}&qkeys[0].target=${Target}&&qkeys[0].key=${
-							this._key
-						}`,
-						headers: { 'Roblox-Place-Id': this._placeId, Cookie: '.ROBLOSECURITY=_||_' + this._cookie },
-						resolveWithFullResponse: true,
-					})
-						.then((v) => {
-							const x = JSON.parse(v.body);
-							return resolve(x.data[0] ? x.data[0].Value : undefined);
-						})
-						.catch((v) => {
-							reject(JSON.parse(v.error).errors[0].message);
-						});
-				}
-			});
-		};
-		public SetAsync = async (Target: string, Value: unknown): Promise<never> => {
-			return new Promise(async (resolve, reject) => {
-				if (this._verb) {
-					await initConnectionWithGP(apis.gamepersistence.replace('/{resource}', ''));
-					r({
-						uri:
-							endpoints.set +
-							`?type=standard&scope=${this._scope || 'global'}&key=${
-								this._key
-							}&target=${Target}&valueLength=${(Value as string).toString().length}`,
-						method: 'POST',
-						form: `&value=${Value}`,
-						headers: { 'Roblox-Place-Id': this._placeId, Cookie: '.ROBLOSECURITY=_||_' + this._cookie },
-						resolveWithFullResponse: true,
-					})
-						.then((v) => {
-							console.info(
-								`> POST /persistence/set?type=standard&scope=${this._scope || 'global'}&key=${
-									this._key
-								}&target=${Target}&valueLength=${
-									(Value as string).toString().length
-								} HTTP/2\n> Host: gamepersistence.roblox.com\n> User-Agent: mfd\n> Accept: */*\n> Content-Type: application/x-www-form-urlencoded\n> Connection: keep-alive\n> \n> &value=${Value}`,
-							);
-							const x = JSON.parse(v.body);
-							console.info(
-								`* TLSv1.3 (IN), TLS handshake, Newsession Ticket (4):\n* TLSv1.3 (IN), TLS handshake, Newsession Ticket (4):\n* old SSL session ID is stale, removing\n* Connection state changed (MAX_CONCURRENT_STREAMS == 100)!`,
-							);
-							console.info(`< HTTP/2 ${v.statusCode}`);
-							Object.keys(v.headers).forEach((value) => console.info(`< ${value}: ${v.headers[value]}`));
-							console.info(`${v.body}* Connection #0 to host gamepersistence.roblox.com left intact`);
-							return resolve(x.data);
-						})
-						.catch((v) => {
-							console.log(v);
-							setTimeout(() => {}, 10000);
-							reject(JSON.parse(v.error).errors[0].message);
-						});
-				} else {
-					r({
-						uri:
-							endpoints.set +
-							`?type=standard&scope=${this._scope || 'global'}&key=${
-								this._key
-							}&target=${Target}&valueLength=${(Value as string).toString().length}`,
-						method: 'POST',
-						form: `&value=${Value}`,
-						headers: { 'Roblox-Place-Id': this._placeId, Cookie: '.ROBLOSECURITY=_||_' + this._cookie },
-						resolveWithFullResponse: true,
-					})
-						.then((v) => {
-							const x = JSON.parse(v.body);
-							return resolve(x.data);
-						})
-						.catch((v) => {
-							reject(JSON.parse(v.error).errors[0].message);
-						});
-				}
-			});
-		};
+	private static legacyDataStore: DataStore;
+	// private static disableUrlEncoding: boolean;
+
+	// private static dataStoreJob: DataStoreJob;
+
+	public static GetDataStore(name: string, scope: string = 'global'): DataStore {
+		checkNameAndScope(name, scope);
+		return this.getDataStoreInternal(name, scope, false, false);
+	}
+	public static GetOrderedDataStore(name: string, scope: string = 'global'): DataStore {
+		checkNameAndScope(name, scope);
+		return this.getDataStoreInternal(name, scope, false, true);
 	}
 }
+class DataStore {
+	constructor(name: string, scope: string /*= 'global'*/, legacy: boolean) {}
+	// private readonly EventSlot = class {
+	// 	public callback: (...args: any[]) => any;
+	// 	public constructor(callback: (...args: any[]) => any) {
+	// 		this.callback = callback;
+	// 	}
+	// 	public fire<Variant extends any>(value: Variant): void {
+	// 		if (typeof value === 'undefined') return;
+	// 		try {
+	// 			this.callback(value);
+	// 		} catch (error) {
+	// 			// TODO: Disconnect?
+	// 			process.stderr.write(error.message);
+	// 		}
+	// 	}
+	// };
+	private isLegacy: boolean = false;
+	private cachedKeys: CachedKeys;
+	private lastSetByKey: KeyTimestamps;
+	private refetchState: RefetchState;
+	private nextKeyToRefetch: string;
+	private constructPostDataForKey(key: string, index: number = 0): string {
+		return this.isLegacy
+			? `&qkeys[${index}].scope=${this.scopeUrlEncodedIfNeeded.toString()}&qkeys[${index}].target=&qkeys[${index}].key=${this.urlEncodeIfNeeded(
+					key,
+			  ).toString()}`
+			: `&qkeys[${index}].scope=${this.scopeUrlEncodedIfNeeded.toString()}&qkeys[${index}].target=${this.urlEncodeIfNeeded(
+					key,
+			  ).toString()}&qkeys[${index}].key=${this.nameUrlEncodedIfNeeded.toString()}`;
+	}
+	private constructGetUrl(): string {
+		const placeId = globals.placeId;
+		return `${
+			this.serviceUrl
+		}getV2?placeId=${placeId}&type=${this.getDataStoreTypeString()}&scope=${this.scopeUrlEncodedIfNeeded.toString()}`;
+	}
+
+	private constructSetUrl(key: string, valueLength: number): string {
+		const placeId = globals.placeId;
+		return this.isLegacy
+			? `${this.serviceUrl}set?placeId=${placeId}&key=${this.urlEncodeIfNeeded(
+					key,
+			  ).toString()}&&type=${this.getDataStoreTypeString()}&scope=${this.scopeUrlEncodedIfNeeded.toString()}&target=&valueLength=${valueLength}`
+			: `${this.serviceUrl.toString()}set?placeId=${placeId}&key=${this.nameUrlEncodedIfNeeded.toString()}&&type=${this.getDataStoreTypeString()}&scope=${this.scopeUrlEncodedIfNeeded.toString()}&target=${this.urlEncodeIfNeeded(
+					key,
+			  ).toString()}&valueLength=${valueLength}`;
+	}
+
+	protected serviceUrl: string;
+	protected name: string;
+	protected scope: string;
+	protected scopeUrlEncodedIfNeeded: string;
+	protected nameUrlEncodedIfNeeded: string;
+	protected checkValueIsAllowed<T>(v?: T): boolean {
+		return true;
+	}
+	protected getDataStoreTypeString(): string {
+		return 'standard';
+	}
+
+	protected urlEncodeIfNeeded(input: string): string {
+		return encodeURIComponent(input);
+	}
+}
+class OrderedDataStore extends DataStore {
+	constructor(name: string, scope: string) {
+		super(name, scope, false);
+	}
+}
+class CachedRecord {
+	private variant: unknown;
+	private serialized: string;
+	private accessTimeStamp: number;
+	public constructor() {}
+	public getVariant<Variant extends any>(touch: boolean /*= true*/): Variant {
+		if (touch) this.accessTimeStamp = Date.now();
+		return this.variant as Variant;
+	}
+	public getSerialized(): string {
+		return this.serialized;
+	}
+	public getTime(): number {
+		return this.accessTimeStamp;
+	}
+	public update<Variant extends any>(variant: Variant, serialized: string): void {
+		this.variant = variant as Variant;
+		this.serialized = serialized;
+	}
+}
+// class DataStoreJob {}
