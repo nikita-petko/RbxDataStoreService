2d4b2c210ecd559d4f8d40c6d60cc3302682c0b6
mfd <minigunneryt@gmail.com>
2020-12-12 15:17:41 +0000

    This function is required to be ran at least once before you use any function, it will setup the cookie and placeId globals.

---
+++ src/Helpers/InitAuthenticatedUser.ts
@@ -0,0 +1,7 @@
+import { checkCookieAndPlaceIdInternalAsync } from './Internal/checkCookieAndPlaceIdInternalAsync';
+import { globals } from '../util/globals';
+
+export const init = async (cookie: string, placeId: number) => {
+	await checkCookieAndPlaceIdInternalAsync(cookie, placeId);
+	globals.cookie = cookie;
+};
