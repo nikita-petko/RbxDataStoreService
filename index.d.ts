bda09d31ba3cebc46b931a6ffce88b738fa7a755
mfd <minigunneryt@gmail.com>
2020-12-19 21:23:52 +0000

    Add this

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+import { _DataStoreService } from './lib/Services/DataStoreService';
+declare const _default: {
+	InitializeAsync: (cookie: string, placeId: number) => Promise<void>;
+	DataStoreService: typeof _DataStoreService;
+};
+export = _default;
