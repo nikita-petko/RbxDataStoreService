6f814aa8b87bdbfdbf02218db1ab12ca783b4919
mfd-co <minigunneryt@gmail.com>
2021-01-01 18:53:42 +0000

    Test

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
-import { _DataStoreService } from './lib/Services/DataStoreService';
+import { _DataStoreService } from './src/Services/DataStoreService';
+
 declare const _default: {
 	InitializeAsync: (cookie: string, placeId: number) => Promise<void>;
 	DataStoreService: typeof _DataStoreService;
 };
-export = _default;
