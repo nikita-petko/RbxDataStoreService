e3ce2817996735a1c3a945fd77f952fecf7fde60
ROBLOX Team Foundation <github@roblox.com>
2021-02-23 21:35:58 +0000

    Add these for the new Roblox-Object-Attributes

---
+++ src/Classes/DataStoreSetOptions.ts
@@ -0,0 +1,14 @@
+export class DataStoreSetOptions {
+	private attributes: Record<string, unknown>;
+	public constructor(attributes?: Record<string, unknown>) {
+		this.attributes = attributes || {};
+	}
+	public GetMetadata() {
+		return this.attributes;
+	}
+	public SetMetadata(attributes: Record<string, unknown>) {
+		if (this.attributes !== attributes) {
+			this.attributes = attributes;
+		}
+	}
+}
