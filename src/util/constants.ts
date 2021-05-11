4bc4b2d8cf8c65bcf67482cb3320989933a4c1d4
Nikita Petko <info@simulpong.cf>
2021-05-11 21:45:47 +0100

    Delete src/util directory

--- src/util/constants.ts
+++
@@ -1,3 +0,0 @@
-export class Constants {
-	public static readonly CookieWarningCapture: RegExp = /_\|WARNING:-DO-NOT-SHARE-THIS\.--Sharing-this-will-allow-someone-to-log-in-as-you-and-to-steal-your-ROBUX-and-items\.\|_/;
-}
