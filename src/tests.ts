d733e99b81a30443af3e7b95cd40c33b07b66d03
ROBLOX Team Foundation <github@roblox.com>
2021-05-11 22:10:34 +0100

    tsc

--- src/tests.ts
+++ src/tests.ts
@@ -7,28 +7,28 @@ DYNAMIC_LOGVARIABLE('Debug', 7);
 (async () => {
 	try {
 		try {
-			FASTLOG(DFLog['Debug'], '[DFLog::Debug] Try authenticate the user.');
+			FASTLOG(DFLog('Debug'), '[DFLog::Debug] Try authenticate the user.');
 			await InitializeAsync(process.argv[2], parseInt(process.argv[3]));
 		} catch (e) {
-			FASTLOGS(DFLog['Debug'], '[DFLog::Debug] Authentication failed because %s, aborting.', e);
+			FASTLOGS(DFLog('Debug'), '[DFLog::Debug] Authentication failed because %s, aborting.', e);
 			return process.exit(1);
 		}
 		FASTLOG(
-			DFLog['Debug'],
+			DFLog('Debug'),
 			'[DFLog::Debug] Attempt to DataStoreService::getDataStore(string, string) with the name of Test.',
 		);
 		const DataStore = DataStoreService.GetDataStore('Test');
-		FASTLOG(DFLog['Debug'], '[DFLog::Debug] Bind a GlobalDataStore::onUpdate for the key TestKey');
+		FASTLOG(DFLog('Debug'), '[DFLog::Debug] Bind a GlobalDataStore::onUpdate for the key TestKey');
 		DataStore.OnUpdate('TestKey', (newValue) => {
 			console.log(newValue);
 		});
-		FASTLOG(DFLog['Debug'], '[DFLog::Debug] Try write a JSON object to the objectKey TestKey.');
+		FASTLOG(DFLog('Debug'), '[DFLog::Debug] Try write a JSON object to the objectKey TestKey.');
 		await DataStore.SetAsync('TestKey', { test: 1 });
-		FASTLOG(DFLog['Debug'], '[DFLog::Debug] Try get the data in Object TestKey');
+		FASTLOG(DFLog('Debug'), '[DFLog::Debug] Try get the data in Object TestKey');
 		console.log(await DataStore.GetAsync('TestKey'));
-		FASTLOG(DFLog['Debug'], '[DFLog::Debug] Try remove the key TestKey from the DataStore');
+		FASTLOG(DFLog('Debug'), '[DFLog::Debug] Try remove the key TestKey from the DataStore');
 		await DataStore.RemoveAsync('TestKey');
-		FASTLOG(DFLog['Debug'], '[DFLog::Debug] Try create the Key TT and increment it by 213');
+		FASTLOG(DFLog('Debug'), '[DFLog::Debug] Try create the Key TT and increment it by 213');
 		await DataStore.IncrementAsync('TT', 213);
 		return process.exit(0);
 	} catch {
